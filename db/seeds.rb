a683083700356a3c7a1fac0d2a020fdc7b107af2
Brian Lambert <brianlambert2408@gmail.com>
2023-02-03 15:06:24 +1100

    User/Role seeeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# Roles
+role_admin = Role.create!(label: "admin", description: "Administrator role, access to everything.")
+Role.create(label: "user", description: "General role, read access only.")
+
+# User
+User.create!(
+    first_name: "Brian", 
+    last_name: "Lambert", 
+    username: "brian", 
+    email: "brian@email.com",
+    password: "password123",
+    role: role_admin
+)
