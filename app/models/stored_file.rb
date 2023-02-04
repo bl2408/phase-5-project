da90c146bb1fac1eaff41077f229681e0d6e1eb0
Brian Lambert <brianlambert2408@gmail.com>
2023-02-04 21:03:45 +1100

    Allow storedfile url and purge on destroy

--- app/models/stored_file.rb
+++ app/models/stored_file.rb
@@ -1,4 +1,6 @@
 class StoredFile < ApplicationRecord
+  before_destroy :purge_file
+  
   belongs_to :collection
 
   has_many :taggables, as: :target, dependent: :destroy
@@ -6,4 +8,13 @@ class StoredFile < ApplicationRecord
 
   has_one_attached :file
 
+  def url
+    file.url
+  end
+
+  private
+
+  def purge_file
+    file.purge
+  end
 end
