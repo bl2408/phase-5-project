91d692f79dfb5d48e9d50506250915b7583cb953
Brian Lambert <brianlambert2408@gmail.com>
2023-02-08 14:04:09 +1100

    category/tags added

--- app/serializers/admin/post_all_serializer.rb
+++ app/serializers/admin/post_all_serializer.rb
@@ -1,3 +1,12 @@
 class Admin::PostAllSerializer < ActiveModel::Serializer
-  attributes :id, :title, :status, :slug, :publish_datetime
+  attributes :id, :title, :status, :slug, :publish_datetime, :category, :tags
+
+  def category 
+    object.category.as_json(only: [:id, :label, :slug])
+  end
+
+  def tags 
+    object.tags.as_json(only: [:id, :label, :slug])
+  end
+
 end
