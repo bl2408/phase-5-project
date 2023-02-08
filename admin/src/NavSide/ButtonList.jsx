26149ed5e7e56686f0c2ea581d7fbd3dfa6e7553
Brian Lambert <brianlambert2408@gmail.com>
2023-02-08 11:48:12 +1100

    styling all post rows

--- admin/src/NavSide/ButtonList.jsx
+++ admin/src/NavSide/ButtonList.jsx
@@ -1,4 +1,4 @@
-import { faHome, faEdit, faListSquares, faTag, faCogs, faFolderOpen } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faListSquares, faTag, faCogs, faFolderOpen, faFileEdit, faFileCirclePlus, faFolderTree } from '@fortawesome/free-solid-svg-icons'
 import NavSideButton from "./NavSideButton";
 
 
@@ -6,9 +6,9 @@ export default function ButtonList(){
     return (
         <> 
             <NavSideButton text="Dashboard" icon={faHome} to="/" />
-            <NavSideButton text="Posts" icon={faEdit} to="posts/" />
+            <NavSideButton text="Posts" icon={faFileEdit} to="posts/" />
             <NavSideButton text="Collections" icon={faFolderOpen} to="collections/" />
-            <NavSideButton text="Categories" icon={faListSquares} to="categories/"/>
+            <NavSideButton text="Categories" icon={faFolderTree} to="categories/"/>
             <NavSideButton text="Tags" icon={faTag}  to="tags/"/>
             <NavSideButton text="Settings" icon={faCogs} to="settings/"/>
         </>
--- admin/src/pages/PostsAll.jsx
+++ admin/src/pages/PostsAll.jsx
@@ -5,6 +5,9 @@ import { getPostsAll } from "../Slices/postsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { displayDate } from "../fns";
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash, faEdit, faInfoCircle } from '@fortawesome/free-solid-svg-icons'
+
 export default function PostsAll(){
 
     const posts = useSelector(state=>state.posts)
@@ -35,13 +38,14 @@ export default function PostsAll(){
 
     const displayPosts = posts.items?.map(post =>{
         const { id, title, status, publish_datetime, slug } = post;
-        const controls = (
-            <>
-                <button className="btn secondary">Edit</button>
-                <button className="btn red">Delete</button>
-            </>
-        )
-        return templateRow(id, [displayDate(publish_datetime), title, status, controls]); 
+        // const controls = (
+        //     <div className="controls">
+        //         <button className="btn-sml secondary"><FontAwesomeIcon icon={faInfoCircle}/></button>
+        //         <button className="btn-sml secondary"><FontAwesomeIcon icon={faEdit}/></button>
+        //         <button className="btn-sml red"><FontAwesomeIcon icon={faTrash} /></button>
+        //     </div>
+        // )
+        return templateRow(id, [title, displayDate(publish_datetime), status, slug/*controls*/]); 
     });
 
 
@@ -54,7 +58,7 @@ export default function PostsAll(){
                     <div>Date/Time</div>
                     <div>Title</div>
                     <div>Status</div>
-                    <div>Controls</div>
+                    <div>Slug</div>
                 </div>                
                 {displayPosts}
                 <div className="bottom">
