4b82bfc213b97c5c60f143c0a97d53dbcdad3fe5
Brian Lambert <brianlambert2408@gmail.com>
2023-02-17 14:26:35 +1100

    Hooks folder

--- admin/src/Notifications/Notifications.jsx
+++ admin/src/Notifications/Notifications.jsx
@@ -1,25 +1,13 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import Notif from "../Components/Notif";
 import "../css/notifications.css"
-import { add } from "../Slices/notificationsSlice";
-import { v4 as uuid } from "uuid";
 export default function Notifications(){
 
     const notifications = useSelector(state=>state.notifications.items)
-    const dispatch = useDispatch();
-
-    const test = ()=>{
-        dispatch(add({
-            id: uuid(),
-            mode: Math.floor(Math.random() * 3)
-        }))
-    };
-
 
     return (
         <div id="notifications-main" >
-            <div className="display thin-scroll">  
-            <button type="button" onClick={test}>TEST</button>   
+            <div className="display thin-scroll">   
                 {
                     notifications.map(item=>(
                         <Notif 
