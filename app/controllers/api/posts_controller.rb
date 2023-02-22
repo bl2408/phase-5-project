97932ae26605d9eca41f4b688c78862bd641f646
Brian Lambert <brianlambert2408@gmail.com>
2023-02-22 13:26:58 +1100

    Format TextCollect for post show

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -7,14 +7,33 @@ class Api::PostsController < Api::ApplicationController
         content = ActiveSupport::JSON.decode(post.content)
 
 
-        content = content.map { |c| {type: c["t"], name: c["bn"], value: c["v"]} }
+        content = content.map do | c |
+            value = c["v"]
+
+            if c["t"] == "textcollect"
+                value = { text: value["t"], collection: value["c"] }
+            end
+
+            {
+                type: c["t"],
+                name: c["bn"], 
+                value: value
+            }
+        end
+        
+
 
         content.each do |c|
             if c[:type] == "collection"
-                c[:value] = c[:value].map do | coll_item |
-                    sf = StoredFile.find_by(id: coll_item["id"])
-                    {alt_text: sf.alt_text, url: sf.file.url, tags: sf.tags.as_json(only: [:label, :slug])}
-                end
+                c[:value] = parse_collection c[:value]
+            elsif c[:type] == "textcollect"
+
+                puts c[:value][:collection]
+
+                c[:value] = {
+                    **c[:value],
+                    collection: parse_collection(c[:value][:collection])
+                }
             end
         end
 
@@ -28,6 +47,15 @@ class Api::PostsController < Api::ApplicationController
         )
 
 
+    end
+
+    private
+
+    def parse_collection value
+        value.map do | coll_item |
+            sf = StoredFile.find_by(id: coll_item["id"])
+            {alt_text: sf.alt_text, url: sf.file.url, tags: sf.tags.as_json(only: [:label, :slug])}
+        end
     end
 
 end
